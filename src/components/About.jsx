aa9b741fa515e517d92e4d0825425f32388bbcb7
mustafa-motiwala07 <mustu.motiwala03@gmail.com>
2024-02-10 01:20:23 +0530

    Add eyes animation

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black '>
+        <h1 className='font-["Neue_Montreal"] text-6xl leading-[4vw] tracking-tighter'>
+            Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.
+        </h1>
+        <div className="w-full flex gap-5 border-t-[1px] pt-10 mt-20 border-[#91a349]">
+        <div className='w-1/2 '>
+            <h1 className='text-7xl'>
+                Our approach:
+            </h1>
+            <button className='flex uppercase gap-10 items-center px-10 py-6 bg-zinc-900 mt-10 rounded-full text-white'>
+                Read More
+                <div className='w-2 h-2 bg-zinc-100 rounded-full'></div>
+            </button>
+        </div>
+        <div className='w-1/2 h-[70vh] bg-[#b0c859] rounded-3xl'></div>
+        </div>
+    </div>
+  )
+}
+
+export default About;
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,10 +4,10 @@ function Marquee() {
   return (
     <div className='w-full py-20 bg-[#004D43] rounded-tl-3xl rounded-tr-3xl'>
       <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden">
-        <h1 className='text-[22vw] leading-none font-["Neue Montreal"] uppercase -mb-15 pb-10 font-medium tracking-tighter'>
+        <h1 className='text-[22vw] leading-none font-["Neue Montreal"] uppercase mb-[3vw] font-medium tracking-tighter'>
             we are ochi
         </h1>
-        <h1 className='text-[22vw] leading-none font-["Neue Montreal"] uppercase -mb-15 pb-10 font-medium tracking-tighter'>
+        <h1 className='text-[22vw] leading-none font-["Neue Montreal"] uppercase mb-[3vw] font-medium tracking-tighter'>
             we are ochi
         </h1>
       </div>
@@ -15,4 +15,4 @@ function Marquee() {
   )
 }
 
-export default Marquee
+export default Marquee;
